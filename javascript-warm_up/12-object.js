11cf4438c3e53da5b6101b63fc2c69579a90da33
Shemaremy <remyshema20@gmail.com>
2024-01-27 22:06:13 +0200

    commit 332

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+// THE QUESTION IS TO UPDATE THE VALUE FROM 12 TO 89, SO THAT THE SECOND OUTPUT WON'T BE IDENTICAL AS THE 1st ONE
+
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+  
+  
+  myObject.value = 89;
+  console.log(myObject);
