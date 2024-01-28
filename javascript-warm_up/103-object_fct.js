07c86fe335d03779d40a8d646ffeb3fb0c531144
Shemaremy <remyshema20@gmail.com>
2024-01-28 23:52:12 +0200

    commit 768

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,24 @@
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+  // Output: { type: 'object', value: 12 }
+  
+
+
+  myObject.incr = function () {
+    this.value++;
+  };
+  myObject.incr();
+  console.log(myObject);
+  // Output: { type: 'object', value: 13 }
+  
+  myObject.incr();
+  console.log(myObject);
+  // Output: { type: 'object', value: 14 }
+  
+  myObject.incr();
+  console.log(myObject);
+  // Output: { type: 'object', value: 15 }
+  
