9f84832467bbfc15e939f23844b6f433f7b2bd28
Shemaremy <remyshema20@gmail.com>
2024-01-31 09:24:44 +0200

    commit 988

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,16 +3,13 @@ class Rectangle
 {
     constructor (w, h)
     {
-        if (w <= 0 || h <= 0)
-        {
-            return {};
-        }
-
-        else
+        if (w > 0 || h > 0)
         {
             this.width = w
-            this.height = h
+            this.height = h   
+             
         }
+        
 
     }
 
