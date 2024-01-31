7a4826af8add52c260927ba7f65008e36f05539f
Shemaremy <remyshema20@gmail.com>
2024-01-31 10:44:53 +0200

    commit 323

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+const Rectangle = require('./5-square')
+
+class Square extends Rectangle
+{
+    constructor(side)
+    {
+        super(side,side)
+    
+    }
+    
+    charPrint(c)
+    {
+        if (c == undefined)
+        {
+            c = 'X'
+        }
+         for (let i=0; i < this.height; i++)
+         {
+            console.log(c.repeat(this.width))
+         }
+
+    }
+
+}
