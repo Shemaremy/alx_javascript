cc60ae70ffdd0eb724f653d0f476f3e53442f517
Shemaremy <remyshema20@gmail.com>
2024-01-31 08:59:50 +0200

    commit 434

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+class Rectangle
+{
+    constructor (w, h)
+    {
+        if (w && h > 0)
+        {
+            this.width = w
+            this.height = h   
+
+            return this.width, this.width
+        }
+    }
+
+};
+
+module.exports = Rectangle
